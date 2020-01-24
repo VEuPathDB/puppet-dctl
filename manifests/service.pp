9d47ba7ee14de6b2db3a511f6968784628122f60
Bob Belnap <robert.belnap@uga.edu>
2020-01-24 09:49:26 -0500

    add project arg to service

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,13 +2,8 @@
 #
 # A description of what this defined type does
 
-# the intent here:
-# this will create the structure for a docker-compose service/project that includes templates for:
-#    docker-compose.yaml           - base docker-compose
-#    docker-compose-dctl.yaml      - overrides for User defined ("shell" template for use by dctl )
-#    docker-compose-[service].yaml - overrides for [service]    (epp template for use by puppet )
-#
-# this will also ensure that the defined service is running 
+# this ensure that the service template is populated, and that the defined
+# service is running 
 
 #
 # @example
@@ -16,15 +11,17 @@
 
 
 define dctl::service (
+  String $project,
   Hash $compose_hash,
 ) {
 
   # TODO this should be done as a project, with some kind of dependency
-  file { "/var/lib/docker-compose/projects/solr/":
-    ensure => directory,
-  }
+  #  file { "/var/lib/docker-compose/projects/solr/":
+  #    ensure => directory,
+  #  }
 
-  file { "/var/lib/docker-compose/projects/solr/docker-compose-${name}.yml":
+  # TODO use main var
+  file { "/var/lib/docker-compose/projects/${project}/docker-compose-${name}.yml":
     ensure  => file,
     # content => to_yaml($compose_hash), # this apparently is too simple to work :/
     content => inline_template( '<%= @compose_hash.to_yaml %>' ),
