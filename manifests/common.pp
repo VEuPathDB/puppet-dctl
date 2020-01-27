e200cbacf69908817e1ac1201be3545b8132ff9a
Bob Belnap <robert.belnap@uga.edu>
2020-01-27 08:43:41 -0500

    add paramater wiring

--- manifests/common.pp
+++ manifests/common.pp
@@ -2,30 +2,30 @@ class dctl::common {
   include '::docker'
   include '::docker::compose'
 
-  $docker_compose_project_root = '/var/lib/docker-compose'
-
-  # this may need to move to a higher level, if we are going
-  # to manage them centrally
-  file { [ $docker_compose_project_root, "${docker_compose_project_root}/projects" ] :
+  # create directory structure
+  file { [ $dctl::docker_compose_dir, "${$dctl::docker_compose_dir}/${dctl::project_dir}" ] :
     ensure => 'directory',
     owner  => 'root',
     group  => 'root',
     mode   => '0700',
   }
 
-  # dctl cli requires python36-docker module
-  package { 'python36-docker':
-    ensure   => installed
-  }
+  if ( $dctl::dctl_cli_enable ) {
+    # dctl cli requires python36-docker module
+    package { 'python36-docker':
+      ensure   => installed
+    }
 
-  $sudo_rule = @(END)
+    $sudo_rule = @(END)
 User_Alias DOCKER_DEV_GROUP = %eupa
 DOCKER_DEV_GROUP ALL=(root) NOPASSWD:/usr/local/bin/dctl
-  END
+END
 
-  sudo::conf { 'dev_docker_dctl':
-    content  => $sudo_rule,
-    priority => 16,
+    sudo::conf { 'dev_docker_dctl':
+      content  => $sudo_rule,
+      priority => 16,
+    }
+
   }
 
 }
