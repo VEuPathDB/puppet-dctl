40572ab279c9fc259eadd370d24b45218e090253
Bob Belnap <robert.belnap@uga.edu>
2020-01-27 08:44:34 -0500

    add commaas

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,8 @@
 
 class dctl (
-  $dctl_cli_enable    = $dctl::params::dctl_cli_enable
-  $docker_compose_dir = $dctl::params::docker_compose_dir
-  $project_dir        = $dctl::params::project_dir
+  $dctl_cli_enable    = $dctl::params::dctl_cli_enable,
+  $docker_compose_dir = $dctl::params::docker_compose_dir,
+  $project_dir        = $dctl::params::project_dir,
 ) inherits dctl::params {
   contain 'dctl::common'
 }
