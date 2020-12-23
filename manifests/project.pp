2943b3c21ff1661e32a9d3320189647d2e5c6f83
Bob Belnap <robert.belnap@uga.edu>
2020-12-23 16:21:44 -0500

    remove git_ prefix from project/service

--- manifests/project.pp
+++ manifests/project.pp
@@ -1,49 +1,35 @@
-# @summary create a docker-compose project which includes templates for compose files & environment
+# @summary create a docker-compose project 
 #
-
-# this will create the structure for a docker-compose service/project that includes templates for:
-#    docker-compose.yaml           - base docker-compose
-#    docker-compose-dctl.yaml      - overrides for User defined ("shell" template for use by dctl )
-#    docker-compose-[service].yaml - overrides for [service]    (epp template for use by puppet )
-#
+# this will create the structure for a docker-compose service/project.
+# It is mainly used to create the directory structure used by service,
+# however, it also provides defaults that can be used by service resources.
 
 #
 # @example
 #  dctl::project{'testservice':
-#    docker_compose_base => "puppet:///modules/profiles/testservice/docker-compose.yml",
-#    docker_compose_dctl => "puppet:///modules/profiles/testservice/docker-compose.yml",
-#    docker_compose_service_template => "profiles/testservice/docker-compose-service.epp",
+#    project_url  => "https://raw.githubusercontent.com/ORG/REPO",
+#    compose_hash => "462926ba1c7fad675ff2e072ef791e070b40a910",
+#    compose_path => "docker-compose.yml",
+#    project_vars => { "KEY" => "VALUE},
 #  }
 
 
 
 define dctl::project (
-  String $docker_compose_base,
-  String $docker_compose_dctl,
-  String $docker_compose_service_template,
+  String $project_url = "",
+  String $compose_hash = "",
+  String $compose_path = "",
+  Hash $project_vars = {},
+  #  Hash $service_vars = {},
 ) {
 
-  # all templates will be deposited here
+  # top level project dir
   $project_dir = "${$::dctl::docker_compose_dir}/${::dctl::project_dir}/${name}"
 
-  # until old-style projects are retired, we need to conditionally check for
-  # existence of this resource, since it is defined both places
+  # create project dir and compose_files storage dir
 
-  if ! defined(File[$project_dir]) {
-    file { $project_dir:
-      ensure => directory,
-    }
+  file { [$project_dir, "${project_dir}/compose_files"]:
+    ensure => directory,
   }
-
-  # base docker-compose.yml
-  file { "${project_dir}/docker-compose.yml":
-    source => $docker_compose_base,
-  }
-
-  # dctl docker-compose template
-  file { "${project_dir}/docker-compose-dctl.yml":
-    source => $docker_compose_dctl,
-  }
-
 
 }
--- manifests/service.pp
+++ manifests/service.pp
@@ -1,85 +1,120 @@
-# @summary create a docker-compose 'service' which includes templates for compose files & environment
-#
-# this ensure that the service template is populated, and that the defined
-# service is running.  Provide a list of images that should be updated before
-# docker-compose is brought up
+# should pull down compose file from git by hash, and create env file from vars
 
-# This uses the naming convention PROJECT_SERVICE when this is used.  The
-# project reference is taken directly from the name.
-#
-# image management is done through the passed in images argument, which *must* specify:
-# * template name - the name used by the template for the image: line in the docker-compose-SERVICE.yml file
-# * image_name    - the name of the image
-# * update        - whether to update the image to the latest version
-#
-# The below example uses the testservice project
+# directory layout per service:
 
-# @example
-#  dctl::service {'testservice_prod':
-#    images => {
-#      'template_name' => 'demo_image',
-#      'image_name' => 'demo_name',
-#      'image_tag'  => 'demo_tag',
-#      'update'     => false }
-#    overrides     => {'domain' => 'bob.com' },
-#    environment   => ['"SOLR_JAVA_MEM=-Xms128m -Xmx128m"'],
-#    update_images => {image => 'example/image', image_tag => 'tag'}
-#  }
+# full_project_path
+#  \_ compose_files
+#    \_ HASH
+#  \_ ${deploy_environment} # (dev/qa/prod)
+#    \_ docker-compose.yml -> ../compose_files/HASH
+#    \_ env
+
+# puppet then just runs:
+# "docker-compose -f ${full_project_path}/env/docker-compose.yml up", 
+# DOCKER_PROJECT_NAME is injected into the env so admins can run
+# "docker-compose up" in $(full_project_path}/env/ for the same thing
+
+# compose only refreshes if docker-compose.yml link changes
+
+# hiera structure:
+
+#dctl::projects:
+#  osigen:
+#    project_url: "https://raw.githubusercontent.com/ORG/REPO"
+#    compose_hash: "46d826b81cefad675ff2e972ef771e078b007910"
+#    compose_path: "docker/docker-compose.yml"
+#    project_vars:
+#      KEY: "default_value"
+#      FOO: "default_bar"
+#      BAZ: "default_ham"
+#
+#dctl::osigen::qa:
+#  osigen_qa:
+#    #compose_hash: "deadbeef" # possible override
+#    service_vars:
+#      KEY: "value"
+#      FOO: "bar"
 
 
 define dctl::service (
-  Hash $overrides = {},
-  Array $images = [],
+  String $project_url = undef,
+  String $compose_hash = "",
+  String $compose_path = "",
+  Hash $project_vars = {},
+  Hash $service_vars = {},
 ) {
 
   include '::docker'
   include '::docker::compose'
 
-  $name_array = split($name, '_') # pull apart name, using PROJECT_SERVICE convention
-  $project = $name_array[0]
-  $service = $name_array[1]
+  # pull apart name, using PROJECT_ENV convention.  This is done this way to
+  # ensure unique names for the services. When using hiera and
+  # create_resources, it is often easier to conform to the PROJECT_ENV standard
+  # in hiera than to try and construct it after the fact in this define
 
+  $name_array = split($name, '_')
+  $project = $name_array[0]
+  $deploy_env = $name_array[1]
+
+
+  # the root of the project
   $full_project_path = "${$::dctl::docker_compose_dir}/${::dctl::project_dir}/${project}"
 
-  # add images to template hash
-  # this looks quite a mess, but just takes the $images array, and sets the 
-  # 'template_name' to 'image_name/image_tag' based on the list of $images given for the service
-  $template_images = $images.reduce({}) |$memo, $value| { $memo.merge( {$value['template_name'] => "${value['image_name']}:${value['image_tag']}" } ) }
+  # merge in project default vars
+  # we also set COMPOSE_PROJECT_NAME to $name so that "docker-compose up /
+  # down" can work from the deploy_env dir
 
-  # merge in image hash
-  $template_hash = merge($overrides, $template_images)
+  $vars = deep_merge($project_vars, ($service_vars + {'COMPOSE_PROJECT_NAME' => $name}))
 
-  # render template for the service
-  file { "${full_project_path}/docker-compose-${service}.yml":
-    ensure  => file,
-    content => epp(Dctl::Project[$project][docker_compose_service_template], $template_hash),
+  # create deploy_env dir
+  file { "${full_project_path}/${deploy_env}":
+    ensure => directory,
   }
 
-  # update any given images
-  $images.each |Hash $image| {
-    if ($image['update']) {
-      docker::image { $image['image_name']:
-        image_tag => $image['image_tag'],
-        before    => Docker_compose[$name],
-      }
-    }
+  # create cached hash file.
+  # we do it this way to not make requests to github every run.  When the file
+  # at that hash is downloaded, it will not change, so this will work fine as
+  # long as the hash file on the server itself is left unmolested.
+
+  $dc_url = "${project_url}/${compose_hash}/${compose_path}"
+  $hash_file = "${full_project_path}/compose_files/${compose_hash}"
+
+  exec{"dc_hash_${name}":
+    command => "curl -o ${hash_file} ${dc_url}",
+    creates => $hash_file
   }
 
-  # TODO this unfortunately restarts the whole project on refresh - would be
-  # nice if it did a nice 'docker-compose up'
+  # Then we ensure the docker-compose.yml in the deploy_env dir links to the correct hash
 
-  # bring compose project up
+  file{ "${full_project_path}/${deploy_env}/docker-compose.yml":
+    ensure  => link,
+    target  => "../compose_files/${compose_hash}",
+    notify  => Docker_compose[$name],
+  }
+
+  # create env file
+
+  $env_template = @(END)
+<% $vars.each |$key, $value| { -%>
+<%= $key -%>=<%= $value %>
+<% } -%>
+END
+
+  file { "${full_project_path}/${deploy_env}/.env":
+    content => inline_epp($env_template, {vars => $vars}),
+    notify  => Docker_compose[$name],
+  }
+
+  # setup docker-compose resource / service
   docker_compose { $name:
     ensure        => present,
     compose_files => [
-      "${full_project_path}/docker-compose.yml",
-      "${full_project_path}/docker-compose-${service}.yml"
+      "${full_project_path}/${deploy_env}/docker-compose.yml",
     ],
-    subscribe     => [
-      File["${full_project_path}/docker-compose.yml"],
-      File["${full_project_path}/docker-compose-${service}.yml"]
-    ],
+    options => ["--env-file", "${full_project_path}/${deploy_env}/.env"],
   }
 
 
 }
+
+
